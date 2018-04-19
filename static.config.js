d5874ffcbd5022ab868f9762461eb94acbb7656c
BoomTech <boomtechny@gmail.com>
2018-04-18 22:47:33 -0400

    Update2

--- static.config.js
+++ static.config.js
@@ -14,13 +14,7 @@ export default {
          <meta name="description" content= "Portfolio Site for BoomTech"/>
   <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
         <title>BoomTechNY</title>
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/site.webmanifest"/>
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-<meta name="msapplication-TileColor" content="#da532c"/>
-<meta name="theme-color" content="#ffffff"/>
+
       </Head>
       <Body>{children}</Body>
     </Html>
